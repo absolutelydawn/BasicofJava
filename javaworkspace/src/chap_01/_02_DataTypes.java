e7cbdcfe49780f727ffa7db698b19a255acf95a1
absolutelydawn <absolutelydawnn@gmail.com>
2024-10-02 13:30:44 +0900

    datagit push origin main

---
+++ javaworkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // string
+        System.out.println("안녕하세요@@");
+        System.out.println(123); // number
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true); // boolean
+        System.out.println(false);
+
+        System.out.println(123+234);
+    }
+}
