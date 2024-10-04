168c33bd7710582eff54ff55251b9d94fec6ff55
absolutelydawn <absolutelydawnn@gmail.com>
2024-10-04 17:27:49 +0900

    Constants

---
+++ javaworkspace/src/chap_01/_06_Constants.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        // 상수
+        final String KR_COUNTRY_CODE = "+82";
+//        KR_COUNTRY_CODE = "8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH = "2024-10-04";
+
+    }
+}
