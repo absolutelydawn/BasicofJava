5f4b9aacb7d33badafd655f5254ad26e34683b42
absolutelydawn <absolutelydawnn@gmail.com>
2024-10-06 13:53:32 +0900

    quiz1

---
+++ javaworkspace/src/chap_01/Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class Quiz_01 {
+    public static void main(String[] args) {
+        String busNum = "상암 08";
+        int countdown = 3;
+        double restDistance = 1.2;
+
+        System.out.println(busNum + "번 버스");
+        System.out.println("약 " + countdown + "분 후 도착");
+        System.out.println("남은 거리 " + restDistance + "km");
+    }
+}
