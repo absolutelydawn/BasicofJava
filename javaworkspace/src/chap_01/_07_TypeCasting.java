589592277e0a58cfa4e78f87ec377e9051fb7ac1
absolutelydawn <absolutelydawnn@gmail.com>
2024-10-06 13:46:46 +0900

    TypeCasting02

--- javaworkspace/src/chap_01/_07_TypeCasting.java
+++ javaworkspace/src/chap_01/_07_TypeCasting.java
@@ -32,5 +32,21 @@ public class _07_TypeCasting {
 
         int convertedScoreInt = (int)score_d; // 191.8 -> 191, 큰범위에서 작은범위로는 자동형변환 안됨
         // double -> float -> long -> int (수동 형변환)
+
+        // 숫자 -> 문자열
+        String s1 = String.valueOf(93);
+        s1 = Integer.toString(93);
+        System.out.println(s1); //93
+
+        String s2 = String.valueOf(98.8);
+        s2 = Double.toString(98.8);
+
+        // 문자열 -> 숫자
+        int i = Integer.parseInt("93");
+        System.out.println(i); // 93
+        double d = Double.parseDouble("98.8");
+        System.out.println(d); // 98.8
+
+       // int error = Integer.parseInt("자바"); // 오류
     }
 }
